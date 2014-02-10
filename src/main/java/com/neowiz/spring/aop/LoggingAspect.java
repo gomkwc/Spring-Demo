cc5837ca8eb1e03b5a2bf19d5a2991ab9407ce40
david100gom <gomkwc@gmail.com>
2014-02-10 09:49:58 +0900

    first commit

--- src/main/java/com/neowiz/spring/aop/LoggingAspect.java
+++ src/main/java/com/neowiz/spring/aop/LoggingAspect.java
@@ -2,11 +2,14 @@ package com.neowiz.spring.aop;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
 /**
  * GE Team - NMP AP
+ *
+ *  AspectJ 설정하기 (설정) > 관점(Aspect) 정의하기 > 포인트컷(Pointcut) 정의하기 > 충고(Advice) 정의하기
  *
  * @author David KIM
  * @since 1.0
@@ -20,7 +23,6 @@ public class LoggingAspect {
         System.out.println("logBefore() is running!");
         System.out.println("hijacked : " + joinPoint.getSignature().getName());
         System.out.println("******");
-
     }
 
     @After("execution(* com.neowiz.spring.demo.service.UserService.getUserInfo(..))")
@@ -28,5 +30,14 @@ public class LoggingAspect {
         System.out.println("logAfter() is running!");
         System.out.println("hijacked : " + joinPoint.getSignature().getName());
         System.out.println("******");
-     }
+    }
+
+    //
+    @AfterReturning(pointcut = "execution(* com.neowiz.spring.demo.service.UserService.getUserInfo(..))",  returning= "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        System.out.println("logAfterReturning() is running!");
+        System.out.println("hijacked : " + joinPoint.getSignature().getName());
+        System.out.println("Method returned value is : " + result);
+        System.out.println("******");
+    }
 }
